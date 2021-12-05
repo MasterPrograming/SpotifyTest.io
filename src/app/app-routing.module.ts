8c2a9ac252a1b64c51197237e6c50565731facc4
Johan Castro <johan.castro@INFODEC.DEV>
2021-12-05 13:27:58 -0500

    Actualizacion 5 diciembre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
  {
   path: '',
-  loadChildren: () => import('./modules/home/home.module.ts').then(m.HomeModule)
+  loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule)
  } 
   
 ];
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from '@shared/components/sidebar/sidebar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',    
+    component: HomePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
